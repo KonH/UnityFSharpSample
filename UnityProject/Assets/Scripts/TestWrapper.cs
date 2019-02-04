80a0d9e07fb4b860259c77451e86a051c8d3e5b2
konh <konh@yandex.ru>
2019-02-04 19:31:46 +0700

    Example of usage in Unity added

---
+++ UnityProject/Assets/Scripts/TestWrapper.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using GameLogics;
+
+public class TestWrapper : MonoBehaviour {
+	public Text       ScoreText;
+	public InputField ScoreAddField;
+	public Button     AddButton;
+	public int        InitialScore;
+
+	Logics.GameState _state;
+
+	Logics.GameState State {
+		get {
+			return _state;
+		}
+		set {
+			_state = value;
+			UpdateView();
+		}
+	}
+
+	private void Awake() {
+		Debug.Assert(ScoreText);
+		Debug.Assert(ScoreAddField);
+		Debug.Assert(AddButton);
+		AddButton.onClick.AddListener(TryToAddScore);
+	}
+
+	void Start() {
+		State = new Logics.GameState(InitialScore);
+		UpdateView();
+	}
+
+	void TryToAddScore() {
+		if ( int.TryParse(ScoreAddField.text, out var value) ) {
+			var result = Logics.addScore(State, value);
+			if ( result != null ) {
+				State = result.Value;
+			} else {
+				Debug.LogError($"Can't add {value} score!");
+			}
+		} else {
+			Debug.LogError($"Can't parse int from '{ScoreAddField.text}'");
+		}
+	}
+
+	void UpdateView() {
+		ScoreText.text = State.score.ToString();
+	}
+}
